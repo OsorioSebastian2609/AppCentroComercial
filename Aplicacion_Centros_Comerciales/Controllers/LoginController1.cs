9d5960fa88912f6291badb7bda25ffbc8ce499d6
cesar daniel rodriguez <cesar.rodriguezg@usantotomas.edu.co>
2022-11-28 11:32:21 -0500

    Se realiza prueba del metodo de Datos donde se reciben datos

--- Aplicacion_Centros_Comerciales/Controllers/LoginController1.cs
+++ Aplicacion_Centros_Comerciales/Controllers/LoginController1.cs
@@ -11,14 +11,22 @@ namespace Aplicacion_Centros_Comerciales.Controllers
             return View();
         }
         public ActionResult LoginViewdos()
+        
         {
             return View("Formulario");
         }
+        [HttpPost]  
         public ActionResult Datos(string Correo, string contraseña)
         {
             UsuarioModel Usuario = new UsuarioModel();
-            bool TipoUsuario = false;
-
+            if((Usuario.Correo==Correo) &&(Usuario.Contraseña==contraseña))
+            {
+                return View();
+            }
+            else
+            {
+                return View();
+            }
             return View();
         }
         
